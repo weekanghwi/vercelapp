4d8221935caa4420158cdc1e2c86bbd5effef4ef
위강휘 <akirasujin@gmail.com>
2023-07-17 11:41:44 +0800

    initial2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,38 @@
 <script>
 	import '../app.postcss';
+	
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte'
+
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">MyApp</span>
+  </NavBrand>
+  <div class="flex items-center md:order-2">
+    <Avatar id="avatar-menu" src="/" />
+    <NavHamburger on:click={toggle} class1="w-full md:flex md:w-auto md:order-1"/>
+    
+    <DarkMode/>
+  </div>
+  <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+    <DropdownHeader>
+    <span class="block text-sm"> Bonnie Green </span>
+    <span class="block truncate text-sm font-medium"> name@flowbite.com </span>
+    </DropdownHeader>
+    <DropdownItem>Dashboard</DropdownItem>
+    <DropdownItem>Settings</DropdownItem>
+    <DropdownItem>Earnings</DropdownItem>
+    <DropdownDivider />
+    <DropdownItem>Sign out</DropdownItem>
+  </Dropdown>
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/services">Services</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
-<script lang="ts">
-  import { Alert } from 'flowbite-svelte';
+<script>
+  import { Indicator } from 'flowbite-svelte'
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
-</div>
+<Indicator color="gray"/>
+<Indicator color="dark"/>
+<Indicator color="orange"/>
+<Indicator color="blue"/>
+<Indicator color="green"/>
+<Indicator color="red"/>
+<Indicator color="purple"/>
+<Indicator color="indigo"/>
+<Indicator color="yellow"/>
+<Indicator color="teal"/>
